5ae8986d53f2fbfb51615b5db6ac9ac48d29b5d8
Yakov Chibizov <chibizov96@gmail.com>
2021-03-08 11:29:33 +0300

    Lesson 3 done
    5 tasks

---
+++ Lesson_3_3.py
@@ -0,0 +1,23 @@
+# В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
+
+import random
+
+N = 10
+MIN_ITEM = -800
+MAX_ITEM = -750
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(N)]
+print(array)
+
+idx_min = 0
+idx_max = 0
+for i in range(len(array)):
+    if array[i] < array[idx_min]:
+        idx_min = i
+    elif array[i] > array[idx_max]:
+        idx_max = i
+
+array[idx_min], array[idx_max] = array[idx_max], array[idx_min]
+print(array)
+
+print('=' * 20)
+
